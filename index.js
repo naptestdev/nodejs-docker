e065a9c53ddab0193db6e34428087714d359ff2c
napthedev <phongna.dev@gmail.com>
2022-03-26 16:20:28 +0700

    Feat: Add new message

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 require("dotenv/config");
 
 app.get("/", (req, res) => {
-  res.send(`Hello world from docker. Your origin is ${req.get("host")}`);
+  res.send(
+    `Hello world from docker. Testing github actions. Your origin is ${req.get(
+      "host"
+    )}`
+  );
 });
 
 const port = process.env.PORT || 5000;
