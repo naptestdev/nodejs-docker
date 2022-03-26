cef117b105b65d3ff5dd7e79ec9c5d61577fb9dd
napthedev <phongna.dev@gmail.com>
2022-03-26 15:41:53 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+require("dotenv/config");
+
+app.get("/", (req, res) => {
+  res.send(`Hello world from docker. Your origin is ${req.get("host")}`);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
